5ea5c47fa58bfeface2df7aa1f6f9b2a45e6bfdf
luclet <110059750+luclet@users.noreply.github.com>
2022-10-05 16:20:42 +0200

    Data Upload, First code structure

--- Code/Start.py
+++ Code/Start.py
@@ -1,1 +1,17 @@
-print("1")
+#Notes:
+#For each Anomaly strategy, we should have the relative valuation
+#in the form of B/M ratio of unterlying stocks.
+#bm is the difference of B/M between Port 10 and Port 1
+
+###
+#Proposed Code To Dos:
+#1.
+#-Get acustomed to data
+#-divide data into first half / second half for OOS
+#-calculate market beta of portfolios
+#-market adjust data returns and bm ratios? (if not done already)
+#-rescale market-adjusted returns and bm ratios, so the variance is equal across anomalies
+#2.
+#-Construct PCs of 50 Portfolios
+#Table 1 (p. 18) would be the aggregate using the rescaling weights?
+
---
+++ Data/FT_portfolio_sorts-monthly-05FEB2020/readme.txt
@@ -0,0 +1,14 @@
+Please cite Haddad, Kozak, Santosh (2020) “Factor Timing” and Giglio, Kelly, Kozak (2020) "Equity Term Structures without Dividend Strips Data" if you use these data.
+
+File naming convention:
+
+ret10_ANOM.csv: value-weighted returns on each of the p1-p10 portfolios for an anomaly ANOM
+n10_ANOM.csv: number of firms in each portfolio
+bmc10_ANOM.csv: book-to-market ratios of portfolios
+totalme10_ANOM.csv: total market capitalization of each portfolio
+
+Variable description:
+
+date: current date
+p1-p10: corresponding variables for each of the p1 (short) -- p10 (long) portfolios
+
